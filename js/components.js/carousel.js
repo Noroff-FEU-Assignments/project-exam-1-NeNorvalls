438774c742c7913eae5f08b13a01094dc2d6082f
Ettenytten <honeygeeh27@gmail.com>
2022-05-06 03:59:40 +0200

    commit

--- js/components.js/carousel.js
+++ js/components.js/carousel.js
@@ -1,78 +1,63 @@
-const url = "https://nenorvalls.no/flower-power/nenorvalls-blog/wp-json/wp/v2/posts?per_page=20&orderby=date&embed";
-const carouselContainer = document.querySelector(".carousel")
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
+
+/* CAROUSEL */
+
+/* API call and create HTML */
+
+const sliderContainer = document.querySelector(".blogs_container")
 
 
-async function callApi(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+async function getData() {
 
-    return data;
-}
+    const response = await fetch("https://nenorvalls.no/flower-power/nenorvalls-blog/wp-json/wp/v2/blogposts?acf_format=standard&orderby=date&_embed");
+    const json = await response.json();
+    console.log(json)
 
-function firstCarouselCard(resource) {
-    const card = document.createElement("div")
-    card.classList.add("carousel-card");
-    card.classList.add("view");
-    card.innerHTML =  `<a href="details.html?id=${resource.id}"><img src="${resource.better_featured_image.source_url}" alt="${resource.title.rendered}"><h3>${resource.title.rendered}</h3></a>`
+    for (let i = 0; i < 6; i++) {
+        sliderContainer.innerHTML += `
+        <a href "#" class="blogs">
+        <div class="img_container">
+        <p class = "recipe_name">${json[i].acf.h1}</p>
+          <img src="${json[i].acf.mainimg}" alt="">
+        </div>
+      </a>
 
-    return card;
+`
 
-}
+        /* Carousel functionality */
 
-function carouselCards(resource) {
-    const card = document.createElement("div")
-    card.classList.add("carousel-card");
-    card.innerHTML =  `<a href="details.html?id=${resource.id}"><img src="${resource.better_featured_image.source_url}" alt="${resource.title.rendered}"><h3>${resource.title.rendered}</h3></a>`
+        const slides = document.querySelectorAll(".blogs")
+        const nextBtn = document.querySelector(".next")
+        const prevBtn = document.querySelector(".prev")
 
-    return card;
+        // prevBtn.style.display = "none"
 
-}
+        let counter = 0;
+        nextBtn.addEventListener("click", () => {
+            console.log(nextBtn)
+            counter++;
+            carouselFunctionality()
+        })
+        prevBtn.addEventListener("click", () => {
+            counter--;
+            carouselFunctionality()
+        })
 
-async function addCarouselCard() {
-    try {
-        const results = await callApi(url)
-        const firstPost = results.slice(0, 1);
-        const restPost = results.slice(1, 6);
-        const postFirst = firstPost.map(firstCarouselCard);
-        const postRest = restPost.map(carouselCards);
-        carouselContainer.append(...postFirst);
-        carouselContainer.append(...postRest);
-        //i'm sure theres a better way than making 2 appends to add view class to only the first one, but atleast ive made it work. Id love some feedback on how to do this better. 
-        carouselContainer.classList.remove("loader")
-    } catch (error) {
-        console.warn(error);
-        carouselContainer.classList.remove("loader");
-        carouselContainer.innerHTML = validateMessage("Failed to collect content!", "error")
+        function carouselFunctionality() {
+            if (counter > 3) {
+                counter = 0;
+            }
+
+            if (counter === -1) {
+                counter = 3;
+            }
+            slides.forEach((slide) => {
+                slide.style.transform = `translateX(-${counter * 100}%)`
+            })
+
+            console.log(counter)
+        }
+
     }
-}
-
-addCarouselCard()
-
-
-function nextCard() {
-    const currentCard = document.querySelector(".carousel-card.view");
-    const nextCard = currentCard.nextElementSibling
-    ? currentCard.nextElementSibling
-    : document.querySelector(".carousel").firstElementChild;
-    currentCard.classList.remove("view");
-    nextCard.classList.add("view");
 
 }
-
-next.addEventListener("click", nextCard);
-
-
-function prevCard() {
-    const currentCard = document.querySelector(".carousel-card.view");
-    const previousCard = currentCard.previousElementSibling
-    ? currentCard.previousElementSibling
-    : document.querySelector(".carousel").lastElementChild;
-    currentCard.classList.remove("view");
-    previousCard.classList.add("view");
-
-}
-
-prev.addEventListener("click", prevCard);
+getData()
