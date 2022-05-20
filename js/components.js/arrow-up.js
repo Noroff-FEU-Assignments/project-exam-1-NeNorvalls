dd7f85af2ea55db075bad1c9c540cec94f9209f7
Ettenytten <honeygeeh27@gmail.com>
2022-05-20 21:10:17 +0200

    commit

---
+++ js/components.js/arrow-up.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+
+    let arrowUp = document.querySelector(".arrow-up");
+    let intervalId = 0;
+
+    const toggleArrow = (e) => {
+
+        if(window.scrollY >= 500) {
+            arrowUp.classList.add("is-block");
+
+            setTimeout(function() {
+                arrowUp.classList.add("is-opacity");
+            }, 10);
+
+        } else {
+            arrowUp.classList.remove("is-opacity");
+        }
+
+    }
+
+    const scrollStep = () => {
+        if (window.pageYOffset === 0) {
+            clearInterval (intervalId);
+        }
+        window.scroll(0, window.pageYOffset - 50);
+    }
+
+    const scrollToTop = () => {
+         intervalId = setInterval(scrollStep, 8.36);
+    }
+
+    // Event listeners
+    arrowUp.addEventListener("click", scrollToTop);
+    window.addEventListener("scroll", toggleArrow);
+    
+}
